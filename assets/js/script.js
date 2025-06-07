e70bc90203318df96c2e492a636292216118a16b
parkdeklnw <6730316121@student.chula.ac.th>
2025-06-07 20:27:27 +0700

    finish header and start popular-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 // JS here
+
+menu = document.querySelector(".menu");
+
+document.querySelector(".hamburger").addEventListener("click",show_hamburger);
+
+function show_hamburger(){
+    menu.classList.toggle("show");
+}
